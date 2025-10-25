914670d7b83c72f78640a43f4f0960b35b5ab7b4
Yingqi LIU <162819777+yqliu617@users.noreply.github.com>
2025-10-25 15:50:54 +0900

    Create abstracts.js

---
+++ assets/js/abstracts.js
@@ -0,0 +1,8 @@
+function toggleAbstract(paperId) {
+  var content = document.getElementById(paperId);
+  if (content.style.display === "none" || content.style.display === "") {
+    content.style.display = "block";
+  } else {
+    content.style.display = "none";
+  }
+}
