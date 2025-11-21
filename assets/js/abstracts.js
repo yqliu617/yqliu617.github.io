b2c16df268577e702fd0514eba8ff270884d4c29
Yingqi LIU <162819777+yqliu617@users.noreply.github.com>
2025-11-21 15:41:41 +0900

    Update abstracts.js

--- assets/js/abstracts.js
+++ assets/js/abstracts.js
@@ -1,8 +1,1 @@
-function toggleAbstract(paperId) {
-  var content = document.getElementById(paperId);
-  if (content.style.display === "none" || content.style.display === "") {
-    content.style.display = "block";
-  } else {
-    content.style.display = "none";
-  }
-}
+
